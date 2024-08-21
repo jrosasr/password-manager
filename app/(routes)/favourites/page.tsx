9d1615ffa07584a66aac1e532d47adf539aee8f8
Juan Rosas <juan.rosasr01@gmail.com>
2024-08-21 08:27:30 -0400

    habilitado listado de favoritos

--- app/(routes)/favourites/page.tsx
+++ app/(routes)/favourites/page.tsx
@@ -1,7 +1,42 @@
-export default function favouritePage() {
+import { getServerSession } from "next-auth"
+import {db} from "@/lib/db"
+
+import { redirect } from "next/navigation"
+import { DataTableItems } from "@/components/Shared/DataTableItems"
+
+export default async function favouritePage() {
+  const session = await getServerSession()
+
+  if (!session || !session.user?.email) {
+    redirect("/login")
+  }
+
+  const user = await db.user.findUnique({
+    where: {
+      email: session?.user.email
+    },
+    include: {
+      elements: {
+        where: {
+          isFavourite: true
+        },
+        orderBy: {
+          createdAt: "desc"
+        }
+      }
+    }
+  })
+
+  if (!user || !user.elements) {
+    redirect("/login")
+  }
+
   return (
     <div>
-      Favourites page
+      <h1>
+        Favourites page
+      </h1>
+      <DataTableItems elements={user.elements} />
     </div>
   )
 }
